33a07f20c0c4c7b00bd0f2f18a0f19514606c3ae
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-02-09 05:42:03 -0800

    add helpers for post and setting text

--- frontend/classes/utils.js
+++ frontend/classes/utils.js
@@ -2,6 +2,10 @@ class Utils {
 
     static fetchLink = "http://localhost:8000/api/definitions/";
     
+    static setText(id, text) {
+        document.getElementById(id).innerHTML = text;
+    }
+
     static async search(word) {
         try {
             const response = await fetch(`${Utils.fetchLink}?word=${word}`);        
@@ -10,6 +14,29 @@ class Utils {
     
         } catch (e) {
             return null;
+        }
+    }
+
+    static async post(word, definition, responseLabel) {
+        try {
+            // Set up XMLHttp request
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", Utils.fetchLink);
+            xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+            // Set up what to do after response
+            xhr.onload = () => {
+                if (xhr.readyState !== 4 || xhr.status !== 200) return null;
+                // Set the response label
+                const message = JSON.parse(xhr.response).message;
+                if (message) responseLabel.innerHTML = message;
+                else responseLabel.innerHTML = "An error occurred, could not store definition to server";
+            }
+
+            xhr.send(JSON.stringify({ word: word, definition: definition }));
+
+        } catch (e) {
+            responseLabel.innerHTML = `Error occurred: ${e}`;
         }
     }
 
