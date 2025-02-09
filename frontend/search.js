23432bbc5290f5b11dd2edd5dd544fd7c029268d
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-02-09 03:42:19 -0800

    rename file to search and use new utility class functions

---
+++ frontend/search.js
@@ -0,0 +1,22 @@
+const searchbar = document.getElementById("searchbar");
+const wordLabel = document.getElementById("word");
+const definitionLabel = document.getElementById("definition");
+
+document.getElementById("submit").addEventListener("click", async (event) => {
+    // Stop page from refreshing
+    event.preventDefault();
+
+    const word = searchbar.value;
+    if (!Utils.isValid(word)) {
+        definitionLabel.innerText = "Search cannot be empty and cannot contain numbers";
+        return;
+    }
+    Utils.clearElements(wordLabel, definitionLabel);
+
+    const result = await Utils.search(word);
+    if (result) {
+        wordLabel.innerHTML = word;
+        definitionLabel.innerHTML = result.message;
+
+    } else definitionLabel.innerHTML = "An error occurred, could not retrieve data from server";
+});
